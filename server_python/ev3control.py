4ae0512eb3e24f108a7e17160507ffd73dd9f91a
paulosell <sellpaulof@gmail.com>
2018-04-24 01:11:29 -0300

    encapsulando movimentacao

---
+++ server_python/ev3control.py
@@ -0,0 +1,28 @@
+#ev3control.py
+from ev3dev.ev3 import *
+from time import sleep
+
+m_esq = LargeMotor('outA')
+m_dir = LargeMotor('outD')
+class EV3Manual(object):
+    #def __init__(self, motor1, motor2):
+     #   m_esq = LargeMotor(motor1)
+      #  m_dir = LargeMotor(motor2)
+
+    def re(self):
+        m_esq.run_timed(time_sp=3000, speed_sp=-100)
+        m_dir.run_timed(time_sp=3000, speed_sp=-100)
+        sleep(2)
+
+    def frente(self):
+        m_esq.run_timed(time_sp=3000, speed_sp=100)
+        m_dir.run_timed(time_sp=3000, speed_sp=100)
+        sleep(2)
+
+    def direita(self):
+        m_esq.run_timed(time_sp=3800, speed_sp=90)
+        sleep(2)
+
+    def esquerda(self):
+        m_dir.run_timed(time_sp=3800, speed_sp=90)
+        sleep(2)
