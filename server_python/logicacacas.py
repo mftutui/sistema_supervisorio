bbd59bb9dd418a49d309c64628d2f170c6a523cd
mftutui <tutuimf@gmail.com>
2018-06-19 14:58:35 -0300

    nova logica pra procura de caças

--- server_python/logicacacas.py
+++ server_python/logicacacas.py
@@ -1,174 +1,234 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 from math import sqrt
+from ev3dev.ev3 import *
+from time import sleep
 
-class Pit:
+m_esq = LargeMotor('outA')
+m_dir = LargeMotor('outD')
 
-    def demais_cacas(self, novo_xA, novo_yA, lista_ordenada):
-        novo_lista_distancias = []
+class Jogo:
 
-        # Proximas caças
-        while len(lista_ordenada) != 0: # Tem que receber da funcao primeira_caca
+    def __init__ (self):
+        self.lista_inicial = ["1:3,5", "2:3,3", "3:5,3", "4:5,5", "5:2,3"]
 
-            # Ordena a lista com -1 posicao
-            for item in lista_ordenada:
-                # Separa as informacoes da(s) linha(s)
-                novo_indice = str(item[4])
+    def re(self):#
+        m_esq.run_timed(time_sp=3500, speed_sp=-90)
+        m_dir.run_timed(time_sp=3500, speed_sp=-90)
+        sleep(3)
 
-                novo_ponto = str(item.split(":")[1])
-                novo_item = (novo_indice + ":" + novo_ponto)
-                novo_sxB = int(novo_ponto.split(",")[0])
-                novo_syB = int(novo_ponto.split(",")[1])
+    def frente(self):
+        m_esq.run_timed(time_sp=3500, speed_sp=90)
+        m_dir.run_timed(time_sp=3500, speed_sp=90)
+        sleep(3)
 
-                # Calcula a(s) distancia(s)
-                novo_distAB = str(round(sqrt(((novo_sxB - novo_xA)**2) + ((novo_syB - novo_yA)**2)), 1))
-                nova_linha = (novo_distAB + "." + novo_item)
+    def esquerda(self):
+        m_dir.run_timed(time_sp=3700, speed_sp=90)
+        sleep(3)
 
-                # Cria uma nova lista com a(s) distancas(s), incice(s) e ponto(s)
-                if novo_lista_distancias == []:
-                    novo_lista_distancias = [nova_linha]
-                else:
-                    novo_lista_distancias.append(nova_linha)
+    def direita(self):
+        m_esq.run_timed(time_sp=1500, speed_sp=90)
+        m_dir.run_timed(time_sp=1500, speed_sp=90)
+        sleep(3)
+        m_esq.run_timed(time_sp=3500, speed_sp=90)
+        sleep(3)
 
-                # Ordena esta nova lista
-                novo_lista_ordenada = sorted(novo_lista_distancias)
-
-            # Separa a primeira linha da nova lista
-            nova_linha = novo_lista_ordenada[0]
-
-
-            # Manipula as informacoes para da linha
-            novo_indice = str(nova_linha[4])
-            novo_pontoxy = nova_linha.split(":")[1]
-            pontox2 = int(novo_pontoxy.split(",")[0])
-            pontoy2 = int(novo_pontoxy.split(",")[1])
-
-            pontox1 = novo_xA # Tem que receber da funcao primeira_caca
-            pontoy1 = novo_yA
-
-            # Anda pra cima/baixo
-            anda_em_y = pontoy2 - pontoy1
-
-            if anda_em_y != 0:
-                if anda_em_y >= 1:
-                    anda_em_y = str(anda_em_y)
-                    print("Anda " + anda_em_y + " casas pra cima")
-                else:
-                    anda_em_y = str(anda_em_y * (-1))
-                    print("Anda " + anda_em_y + " casas pra baixo")
-            else:
-                print("Não anda nada em y")
-
-            # Anda pra direita/esquerda
-            anda_em_x = pontox2 - pontox1
-
-            if anda_em_x != 0:
-                if anda_em_x >= 1:
-                    anda_em_x = str(anda_em_x)
-                    print("Anda " + anda_em_x + " casas pra direita")
-                else:
-                    anda_em_x = str(anda_em_x * (-1))
-                    print("Anda " + anda_em_x + " casas pra esquerda")
-            else:
-                print("Não anda nada em x")
-
-            novo_xA = pontox2
-            novo_yA = pontoy2
-
-            print("Ponto atual do robo " + str(novo_xA) + "," + str(novo_yA))
-
-            print("Chegou na caça " + novo_indice)
-
-            novo_lista_ordenada.pop(0)
-
-            lista_ordenada = novo_lista_ordenada
-
-            novo_lista_distancias = []
+    def giro(self):
+        m_esq.run_timed(time_sp=4000, speed_sp=-400)
+        m_dir.run_timed(time_sp=4000, speed_sp=400)
+        sleep(2)
+        m_esq.run_timed(time_sp=4000, speed_sp=400)
+        m_dir.run_timed(time_sp=4000, speed_sp=-400)
 
 
     def primeira_caca(self):
 
-        lista_inicial = ["1:3,5", "2:3,3", "3:5,3", "4:5,5", "5:2,3"]
-        lista_distancias = []
+        # Só aux pra gambiarra
+        aux = 0
+
+        # Saber se o robô ja partiu pra varrer o x
+        girou_direita = 0
+        girou_esquerda = 0
+
+        indice_anterior = 0
 
         # Ponto de partida do robo
-        xA = 1
-        yA = 3
+        xA = 0
+        yA = 0
 
-        # Cria uma lista ordenada a partir da primeira lista recebida
-        for item in lista_inicial:
+        # Ainda têm caças a serem pegas
+        while len(self.lista_inicial) != 0:
 
-            # Separa as informacoes das linhas da lista_inicial passada pelo SS
-            indice = int(item.split(":")[0])
-            ponto = str(item.split(":")[1])
-            sxB = int(ponto.split(",")[0])
-            syB = int(ponto.split(",")[1])
+            # Gambiarra pra tirar uma caça no meio da partida
+            if aux == 3:
+                self.lista_inicial = ["1:3,5", "2:3,3", "3:5,3", "4:5,5"]
+                print("A caca 5 ja foi pega!")
+                print("--------------------------------------")
 
-            # Distancia em int por pitagoras
-            distAB = str(round(sqrt(((sxB - xA)**2) + ((syB - yA)**2)), 1))
+            # Zerando a lista com as distancias entre o robô e as caças e a ordenada
+            lista_distancias = []
+            lista_ordenada = []
 
-            # Cria uma nova lista lista_distancias
-            # onde o primeiro parametro é a distancia entre o ponto inicial os demais pontos
-            if lista_distancias == []:
-                lista_distancias = [distAB + "." + item]
+            # Cria uma lista com as distancias a partir da primeira lista recebida
+            for item in self.lista_inicial:
+
+                # Separa as informacoes das linhas da lista_inicial passada pelo SS
+                #indice_lista = int(item.split(":")[0])
+                ponto = str(item.split(":")[1])
+                sxB = int(ponto.split(",")[0])
+                syB = int(ponto.split(",")[1])
+
+                # Distancia em int por pitagoras
+                distAB = str(round(sqrt(((sxB - xA)**2) + ((syB - yA)**2)), 1))
+
+                # Cria uma nova lista lista_distancias
+                # onde o primeiro parametro é a distancia entre o ponto inicial os demais pontos
+                if lista_distancias == []:
+                    lista_distancias = [distAB + "." + item]
+                else:
+                    lista_distancias.append(distAB + "." + item)
+
+                # Ordena essa lista da menor distância para a maior
+                lista_ordenada = sorted(lista_distancias)
+
+            # Separa a primeira linha da lista lista_ordenada
+            # a primeira linha contém o ponto mais proximo do ponto inicial
+            linha_lista = lista_ordenada[0]
+
+            # Separo as informacoes da linha 0 para poder determinar o quanto é necessário andar
+            # para chegar no ponto mais proximo
+            indice = str(linha_lista[4])
+            pontoxy = linha_lista.split(":")[1]
+            xB = int(pontoxy.split(",")[0])
+            yB = int(pontoxy.split(",")[1])
+
+            # Tratando da caças diferente
+            # Já é a segunda rodada e já tenho um indice anterior
+            if aux != 0:
+                # Se tratando de uma nova caça
+                if indice != indice_anterior:
+                    # Se girou pra direita, volta pra esquerda
+                    if girou_direita == 1:
+                        robot.esquerda()
+                        print("Voltou para a esquerda!")
+                        girou_direita = 0
+                    # Se girou pra esquerda, volta pra direita
+                    elif girou_esquerda ==1:
+                        robot.direita()
+                        print("Voltou para a direita!")
+                        girou_esquerda = 0
+
+            indice_anterior = indice
+            # Debug
+            print("A caca " + indice + " e a caca mais proxima e esta no ponto " + pontoxy)
+
+            # Repassando
+            yAinicio = yA
+            xAinicio = xA
+
+            # Andar somente uma casa e depois verificar tudo outra vez
+
+            # Anda pra cima/baixo
+            anda_em_y = yB - yA
+
+            # Vira pra direita/esquerda
+            anda_em_x = xB - xA
+
+            # If para andar em y
+            if anda_em_y != 0:
+                # Se maior ou igual a 1, anda para frente
+                if anda_em_y >= 1:
+                    print("Anda  1 casa pra frente")
+                    yA = yA + 1
+                    print("Estava no ponto " + str(xAinicio) + "," + str(yAinicio))
+                    print("Ponto atual do robo " + str(xA) + "," + str(yA))
+                    robot.frente()
+                # Se menor que 1, anda para tras
+                else:
+                    print("Anda 1 casa pra tras")
+                    yA = yA - 1
+                    print("Estava no ponto " + str(xAinicio) + "," + str(yAinicio))
+                    print("Ponto atual do robo " + str(xA) + "," + str(yA))
+                    robot.re()
+            # Caso a diferença em y seja igual a zero, anda em x
             else:
-                lista_distancias.append(distAB + "." + item)
+                # Se a diferença for diferente de 0
+                if anda_em_x != 0:
+                    # Se maior ou igual a 1, gira pra a direita e anda pra frente
+                    if anda_em_x >= 1:
+                        xA = xA + 1
 
-            # Ordena essa lista da menor distância para a maior
-            lista_ordenada = sorted(lista_distancias)
+                        # Controle do giro, que tem que ser "desfeito" ao fim da procura da caça
+                        # Já girou = 1 ---- Não girou ainda = 0
 
-        # Separa a primeira linha da lista lista_ordenada
-        # a primeira linha contém o ponto mais proximo do ponto inicial
-        linha_lista = lista_ordenada[0]
+                        # Se não girei, giro e ando
+                        if girou_direita == 0:
+                            print("Vai pra direita e pra frente")
+                            girou_direita = 1
+                            robot.direita()
+                            sleep(2)
+                            robot.frente()
+                            sleep(2)
 
-        # Separo as informacoes da linha 0
-        # para poder determinar o quanto é necessário andar
-        # para chegar no ponto mair proximo
-        indice = str(linha_lista[4])
-        pontoxy = linha_lista.split(":")[1]
-        pontox2 = int(pontoxy.split(",")[0])
-        pontoy2 = int(pontoxy.split(",")[1])
+                        # se ja girei, só ando
+                        else:
+                            robot.frente()
+                            sleep(2)
+                            print("So anda pra frente")
 
-        pontox1 = xA
-        pontoy1 = yA
+                        print("Estava no ponto " + str(xAinicio) + "," + str(yAinicio))
+                        print("Ponto atual do robo " + str(xA) + "," + str(yA))
 
-        # Anda pra cima/baixo
-        anda_em_y = pontoy2 - pontoy1
-        if anda_em_y != 0:
-            if anda_em_y >= 1:
-                anda_em_y = str(anda_em_y)
-                print("Anda " + anda_em_y + " casas pra cima")
-            else:
-                anda_em_y = str(anda_em_y * (-1))
-                print("Anda " + anda_em_y + " casas pra baixo")
-        else:
-            print("Não anda nada em y")
+                    # Se menor que 1, gira para a esquerda e anda pra frente
+                    else:
+                        print("Anda 1 casa pra esquerda")
+                        xA = xA - 1
 
-        # Anda pra direita/esquerda
-        anda_em_x = pontox2 - xA
-        if anda_em_x != 0:
-            if anda_em_x >= 1:
-                anda_em_x = str(anda_em_x)
-                print("Anda " + anda_em_x + " casas pra direita")
-            else:
-                anda_em_x = str(anda_em_x * (-1))
-                print("Anda " + anda_em_x + " casas pra esquerda")
-        else:
-            print("Não anda nada em x")
+                        # Controle do giro, que tem que ser "desfeito" ao fim da procura da caça
+                        # Já girou = 0 ---- Não girou ainda = 1
 
-        # Novo ponto atual
-        ponto_atual_x = pontox1 + int(anda_em_x)
-        ponto_atual_y = pontoy1 + int(anda_em_y)
-        print("Ponto atual do robo " + str(ponto_atual_x) + "," + str(ponto_atual_y))
+                        # Se não girei, giro e ando
+                        if girou_esquerda == 0:
+                            girou_esquerda = 1
+                            print("Vai pra esquerda e pra frente")
+                            robot.esquerda()
+                            sleep(2)
+                            robot.frente()
+                            sleep(2)
 
-        # Chehou na caça X
-        print("Chegou na caça " + indice)
+                        # se ja girei, só ando
+                        else:
+                            print("So anda pra frente")
+                            robot.frente()
+                            sleep(2)
 
-        # Tira a linha usada
-        lista_ordenada.pop(0)
+                        print("Estava no ponto " + str(xAinicio) + "," + str(yAinicio))
+                        print("Ponto atual do robo " + str(xA) + "," + str(yA))
 
-        # tem que retornar a lista e o novo ponto X e Y
-        self.demais_cacas(ponto_atual_x, ponto_atual_y, lista_ordenada)
+                # Se a diferença em x seja 0 o robô chegou a caça procurada
+                else:
+                    print("Chegou na caca " + indice)
+                    #print("Estava no ponto " + str(xAinicio) + "," + str(yAinicio))
+                    print("Ponto atual do robo " + pontoxy)
+
+                    # Remove a caça que acabou de encontrar da lista
+                    # (Envia para o SS que envia para o SA e confirma)
+                    self.lista_inicial.remove(indice + ":" + pontoxy)
+
+                    print("--------------------------------------")
+
+            # Tira a linha usada ??
+            #lista_ordenada.pop(0)
+
+            # Não tirar pq está sendo usado para outro fim além da gambiarra
+            aux = aux + 1
+
+        print("Acabou o jogo, todas as cacas foram pegas")
+        robot.giro()
+        Sound.tone([(3000, 2000, 400),(800, 1800, 2000)]).wait()
+        Sound.beep()
+
 
 if __name__ == "__main__":
-    robot = Pit()
+    robot = Jogo()
     robot.primeira_caca()
